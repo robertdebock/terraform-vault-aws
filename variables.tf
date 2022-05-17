59415669a44aa55fe1343c9f1440c21194ba12e3
Robert de Bock <robert@meinit.nl>
2022-05-17 15:24:46 +0200

    TTLs.

--- variables.tf
+++ variables.tf
@@ -5,3 +5,13 @@ variable "access_key" {
 variable "secret_key" {
   description = "The secret key that Vault will use to generate credentials"
 }
+
+variable "max_lease_ttl_seconds" {
+  description = "The maximum TTL that can be requested for credentials issued by this backend."
+  default     = 120
+}
+
+variable "default_lease_ttl_seconds" {
+  description = "The default TTL for credentials issued by this backend."
+  default     = 60
+}
