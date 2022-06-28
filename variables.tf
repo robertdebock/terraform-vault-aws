efa0661ade9c99dedfa6ff34e29e5d737dd295f7
Robert de Bock <robert@meinit.nl>
2022-06-28 10:33:14 +0200

    Add validation.

--- variables.tf
+++ variables.tf
@@ -1,9 +1,17 @@
 variable "access_key" {
   description = "The access key that Vault will use to generate credentials."
+  validation {
+    condition     = can(regex("^A..................Q$", var.access_key)) || var.access_key == null
+    error_message = "When specifying an access key, please use the correct format."
+  }
 }
 
 variable "secret_key" {
   description = "The secret key that Vault will use to generate credentials"
+  validation {
+    condition     = try(length(var.secret_key),"fourtybitsofrandomnessisnoteasytomakeman") == 40 || var.secret_key == null
+    error_message = "When specifying a secret key, please use the correct length."
+  }
 }
 
 variable "max_lease_ttl_seconds" {
